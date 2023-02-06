9960d21e3c4b4507af183d2abd5334fbfe6d986d
Alyona <borisovsk_a@mail.ru>
2023-02-06 17:03:43 +0300

    test: Converters tests complete

--- app/src/test/java/com/example/coroutines/TickerOutputConverterUnitTest.kt
+++ app/src/test/java/com/example/coroutines/TickerOutputConverterUnitTest.kt
@@ -1,5 +1,53 @@
 package com.example.coroutines
 
+import com.example.coroutines.data.converters.TickerOutputConverter
+import com.example.coroutines.data.converters.TickerQueryConverter
+import com.example.coroutines.models.data.TickerQueryData
+import com.example.coroutines.models.domain.Quote
+import com.example.coroutines.models.domain.Ticker
+import com.example.coroutines.models.domain.TickerOutput
+import com.example.coroutines.models.domain.TickerQuery
+import com.google.common.truth.Truth
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
+
 class TickerOutputConverterUnitTest {
-    // TODO
+    @Test
+    fun tickerOutputConverterMock_whenCallingMockedMethod_thenCorrectlyVerified() {
+        val tickerParam = Ticker("country",
+            "currency",
+            "exchange",
+            "finnhubIndustry",
+            "ipo",
+            "logo",
+            0.0,
+            "name",
+            "phone",
+            0.0,
+            "ticker",
+            "weburl")
+        val quoteParam = Quote(0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0)
+
+        val tickerOutput = TickerOutput("logo", "name", "symbol", 0.0, 0.0, 0.0)
+
+        // given
+        val service = mockk<TickerOutputConverter>()
+        every { service.convert(tickerParam, quoteParam) } returns tickerOutput
+
+        // result - actual, tickerOutput - expected
+        val result = service.convert(tickerParam, quoteParam)
+
+        // then
+        verify { service.convert(tickerParam, quoteParam) }
+        Truth.assertThat(result).isEqualTo(tickerOutput)
+    }
 }
--- app/src/test/java/com/example/coroutines/TickerQueryConverterUnitTest.kt
+++ app/src/test/java/com/example/coroutines/TickerQueryConverterUnitTest.kt
@@ -1,5 +1,33 @@
 package com.example.coroutines
 
+import com.example.coroutines.data.converters.QuoteConverter
+import com.example.coroutines.data.converters.TickerQueryConverter
+import com.example.coroutines.models.data.QuoteData
+import com.example.coroutines.models.data.TickerQueryData
+import com.example.coroutines.models.domain.Quote
+import com.example.coroutines.models.domain.TickerQuery
+import com.google.common.truth.Truth
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
+
 class TickerQueryConverterUnitTest {
-    // TODO
+    @Test
+    fun tickerQueryConverterMock_whenCallingMockedMethod_thenCorrectlyVerified() {
+        val tickerQueryDataParam = TickerQueryData("3M Company", "Industrials", "MMM")
+
+        val tickerQueryOutput = TickerQuery("3M Company", "Industrials", "MMM")
+
+        // given
+        val service = mockk<TickerQueryConverter>()
+        every { service.convert(tickerQueryDataParam) } returns tickerQueryOutput
+
+        // result - actual, tickerOutput - expected
+        val result = service.convert((tickerQueryDataParam))
+
+        // then
+        verify { service.convert(tickerQueryDataParam) }
+        Truth.assertThat(result).isEqualTo(tickerQueryOutput)
+    }
 }
